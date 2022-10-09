87ced723064f82d34d63f419bbc970b3dc8056ce
Jiaming Guo <j23guo@gmail.com>
2022-10-09 11:36:16 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Jiaming Guo on 2022-10-09.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
